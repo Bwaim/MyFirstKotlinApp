e0806fea266a0fb6a0d0bbd37e3558620ea1676a
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2018-12-19 22:31:19 +0100

    String templates in kotlin

--- app/src/main/java/com/bwaim/myfirstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/bwaim/myfirstkotlinapp/MainActivity.kt
@@ -18,7 +18,8 @@ package com.bwaim.myfirstkotlinapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -26,10 +27,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        toast("My first Kotlin app !")
 
-        val textView = findViewById<TextView>(R.id.text_view)
-        textView.text = getString(R.string.hello_kotlin)
+        val message = findViewById<EditText>(R.id.message)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener { toast("Hello ${message.text} !") }
+
     }
 
     fun toast(message: String) {
