4344e8776a4d1bc733e1126527d8f769b09d4dcf
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2019-03-04 22:25:23 +0100

    Use collections in kotlin

--- app/src/main/java/com/bwaim/myfirstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/bwaim/myfirstkotlinapp/MainActivity.kt
@@ -19,16 +19,36 @@ package com.bwaim.myfirstkotlinapp
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
-    val recyclerView by lazy { findViewById<RecyclerView>(R.id.recycler) }
+    private val recyclerView: RecyclerView by lazy { findViewById<RecyclerView>(R.id.recycler) }
+    private val adapter = MediaAdapter(getMedia()) { (title) -> toast(title) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerView.adapter = MediaAdapter(getMedia()) { (title) -> toast(title) }
+        recyclerView.adapter = adapter
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        adapter.items = getMedia().let { media ->
+            when (item.itemId) {
+                R.id.filter_all -> media
+                R.id.filter_photos -> media.filter { it.type == MediaItem.Type.PHOTO }
+                R.id.filter_videos -> media.filter { it.type == MediaItem.Type.VIDEO }
+                else -> emptyList()
+            }
+        }
+        return true
     }
 }
